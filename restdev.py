f30a380b4d9e397309e4db20006296a8156bce6f
Sakaauti <H8543@student.jamk.fi>
2016-06-08 13:02:50 +0300

    Added checks and single device get method

--- restdev.py
+++ restdev.py
@@ -30,21 +30,44 @@ class Device(Resource):
 	self.reqparse = reqparse.RequestParser()
 	self.reqparse.add_argument('device', type=str, required=True, help='No device provided', location='json')
 
+    def get(self):
+	args = self.reqparse.parse_args()
+	device = args['device']
+	# Check if parameter is empty
+	if len(device) == 0:
+	    abort(404)
+	# Check if device is allowed
+	for element in devices:
+	    if element['device'] == device:
+	   	 return {'device allowed':'true'}
+	return {'device allowed':'false'}
+
     def post(self):
 	args = self.reqparse.parse_args()
 	device = args['device']
+	# Check if parameter is empty
 	if len(device) == 0:
 	    abort(404)
+	# Check if device is allowed
+	for element in devices:
+	    if element['device'] == device:
+		return {'result': [{'success':'false', 'details':'device already allowed'}]}
 	devices.append({'device': device})	
 	return {'success':'true'}
 
     def delete(self):
 	args = self.reqparse.parse_args()
 	device = args['device']
+	# Check if parameter is empty
 	if len(device) == 0:
 	    abort(404)
-	devices.remove({'device': device})
-	return {'success':'true'}
+	# Check if device is allowed
+	for element in devices:
+	    if element['device'] == device:
+		# Remove device from allowed list
+		devices.remove({'device': device})
+		return {'success':'true'}
+	return {'result': [{'success':'false', 'details':'no such device in allowed list'}]}
 
 api.add_resource(DeviceList, '/network/api/v0.1/devices', endpoint='devices')
 api.add_resource(Device, '/network/api/v0.1/device', endpoint='device')
