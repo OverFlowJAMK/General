bf55527c530decd70214ab9bc03f4390c514a625
Sanna Räisänen <H9574@student.jamk.fi>
2016-08-25 10:52:07 +0300

    Add files via upload

--- Daemon_Client_Server/DaemonClient03_TCP_testaus.py
+++ Daemon_Client_Server/DaemonClient03_TCP_testaus.py
@@ -38,109 +38,136 @@ def ErrorHandling(filu,section,a_info,b_info):
         
 
 def send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port):
-    #try:
-    kierros = 0
-    print("-"*60)
-    s = None
-    for res in socket.getaddrinfo(IP, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        af, socktype, proto, canonname, sa = res
-        try:
-            s = socket.socket(af, socktype, proto)
-        except socket.error as msg:
-            time.sleep(5)
-            print(numbr,msg)
-            s = None
-            continue
-        try:
-            #print(numbr,"*** try connection",sa)
-            s.connect(sa)
-        except socket.error as msg:
-            time.sleep(5)
-            print(numbr,msg)
-            s.close()
-            s = None
-            continue
-        break
-    if s is None:
-        print(numbr,'*** could not open socket')
-        afp = open(afilu, 'r')
-        Config.readfp(afp)
-        PORT = int(Config.get('address','port'))
-        afp.close()
-        send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)    
-    #Lähetetään check        
-    if(PORT==start_port):
-        CHECK = "I am Client"
-        s.sendall(CHECK.encode("utf-8"))
-        #print('*** send check')
-            
-    while True:
-        bfp = open(bfilu, 'r')
-        Config.readfp(bfp) 
-        PAYLOAD = Config.get('KaMU','username')
-        bfp.close()
-        #print ("ip:", IP)
-        #print ("port:", PORT)
-        #print (PAYLOAD)
+    try:
+        kierros = 0
+        #print("-"*60)
+        s = None
+        for res in socket.getaddrinfo(IP, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            try:
+                s = socket.socket(af, socktype, proto)
+            except socket.error as msg:
+                time.sleep(5)
+                print(numbr,msg)
+                s = None
+                continue
+            try:
+                #print(numbr,"*** try connection",sa)
+                s.connect(sa)
+            except socket.error as msg:
+                time.sleep(5)
+                print(numbr,msg)
+                s.close()
+                s = None
+                continue
+            break
+        if s is None:
+            print(numbr,'*** could not open socket')
+            afp = open(afilu, 'r')
+            Config.readfp(afp)
+            PORT = int(Config.get('address','port'))
+            afp.close()
+            send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
+
+        #Send check        
+        if(PORT==start_port):
+            CHECK = "I am Client"
+            s.sendall(CHECK.encode("utf-8"))
+            #print('*** send check')
                 
-        try:
-            s.settimeout(t)
-            key = s.recv(1024)
-            #print(key)
-            s.settimeout(None)
-            if key == b'Good try <3':
-                afp = open(afilu, 'r')
-                Config.readfp(afp)
-                PORT = int(Config.get('address','port'))
-                afp.close()
-                send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
-            kierros = 0
-        except:
-            print(numbr,'*** timeout')
-            kierros = kierros + 1
-            if kierros==5:
+        while True:
+            #bfp = open(bfilu, 'r')
+            #Config.readfp(bfp) 
+            #PAYLOAD = Config.get('KaMU','username')
+            #bfp.close()
+            
+            #print ("ip:", IP)
+            #print ("port:", PORT)
+            #print (PAYLOAD)
+            while True:        
+                try:
+                    s.settimeout(t)
+                    key = s.recv(1024)
+                    #print(key)
+                    s.settimeout(None)
+                    if key == b'Good try <3':
+                        afp = open(afilu, 'r')
+                        Config.readfp(afp)
+                        PORT = int(Config.get('address','port'))
+                        afp.close()
+                        send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
+                    kierros = 0
+                    break
+                except:
+                    #print(numbr,'*** timeout')
+                    kierros = kierros + 1
+                    if(kierros==5):
+                        afp = open(afilu, 'r')
+                        Config.readfp(afp)
+                        IP = Config.get("address","ip")
+                        PORT = int(Config.get("address","port"))
+                        afp.close()
+                        send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
+                    time.sleep(t)
+                    #continue
+                
+            #Salataan ja lahetetaan tieto julkisella avaimella
+            P = dict(json.loads(key.decode("UTF-8")))
+            public_key = jwk.JWK(**P)
+            claims = dict(exp=PAYLOAD)
+            header = dict(alg="RSA-OAEP-256", enc="A128GCM")
+            T = jwt.JWT(header, claims)
+            T.make_encrypted_token(public_key)
+            encrypted_signed_token = T.serialize(compact=True)
+            #print("*** engrypted signed token:",encrypted_signed_token)
+            s.sendall(encrypted_signed_token.encode("utf-8"))
+            #print('*** send inoformation')
+
+            afp = open(afilu, 'r')
+#Miksi alla oleva aiheuttaa errorin?
+            Config.readfp(afp)
+            old_PORT = int(Config.get('address','port'))
+            afp.close()
+
+            if PORT == old_PORT:
+                NEW_PORT = s.recv(1024).decode("utf-8")
+                if PORT == b'':
+                    send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
+
+                else:
+                    s.close()
+                    send_UDP(numbr,afilu,bfilu,t,IP,NEW_PORT,PAYLOAD,start_port)
+            time.sleep(t)
+    except Exception as msg:
+        while True:
+            print("*** Opening",afilu)
+            try:
                 afp = open(afilu, 'r')
                 Config.readfp(afp)
                 IP = Config.get("address","ip")
                 PORT = int(Config.get("address","port"))
                 afp.close()
-                send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
-            time.sleep(t)
-            continue
-            
-        #Salataan ja lähetetään tieto julkisella avaimella
-        P = dict(json.loads(key.decode("UTF-8")))
-        public_key = jwk.JWK(**P)
-        claims = dict(exp=PAYLOAD)
-        header = dict(alg="RSA-OAEP-256", enc="A128GCM")
-        T = jwt.JWT(header, claims)
-        T.make_encrypted_token(public_key)
-        encrypted_signed_token = T.serialize(compact=True)
-        #print("*** engrypted signed token:",encrypted_signed_token)
-        s.sendall(encrypted_signed_token.encode("utf-8"))
-        #print('*** send inoformation')
-
-        afp = open(afilu, 'r')
-        Config.readfp(afp)
-        old_PORT = int(Config.get('address','port'))
-        afp.close()
-
-        if PORT == old_PORT:
-            NEW_PORT = s.recv(1024).decode("utf-8")
-            if PORT == b'':
-                send_UDP(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port)
-            else:
-                s.close()
-                send_UDP(numbr,afilu,bfilu,t,IP,NEW_PORT,PAYLOAD,start_port)
+                break
+            except:
+                print("*** File is missing sections and information.")
+                ErrorHandling(afilu,"address","ip","port")
+                   
+        while True:
+            print("*** Opening",bfilu)   
+            try:
+                bfp = open(bfilu, 'r')
+                Config.readfp(bfp) 
+                PAYLOAD = Config.get("KaMU","username")
+                bfp.close()
+                break
+            except:
+                print("*** File is missing sections and information.")
+                bfp.close()
+                ErrorHandling(bfilu,"KaMU","username","time")
+        start_port=PORT
+        print(numbr,"***New start",msg)
         time.sleep(t)
-    #except Exception as msg:
-        #afp = open(afilu, 'r')
-        #Config.readfp(afp)
-        #old_PORT = int(Config.get('address','port'))
-        #afp.close()
-        #print(numbr,"***New start",msg)
-        #time.sleep(t)
-        #send_UDP(numbr,afilu,bfilu,t,IP,old_PORT,PAYLOAD)
+        send_UDP(numbr,afilu,bfilu,t,IP,old_PORT,PAYLOAD,start_port)
 
 t = 5
 afilu = 'ServerAddress.ini'
@@ -173,8 +200,10 @@ while True:
         ErrorHandling(bfilu,"KaMU","username","time")
 start_port=PORT
 numbr=0
-while True:
+
+
+while(numbr<=750):
     numbr = numbr+1
     print(numbr,"start")
     thread.start_new_thread(send_UDP,(numbr,afilu,bfilu,t,IP,PORT,PAYLOAD,start_port,))
-    time.sleep(1)
+    time.sleep(2)
